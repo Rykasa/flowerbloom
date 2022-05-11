ac64a98d460a101b0614984e1077ae50424ada87
LAK <email@lak.com>
2022-05-11 15:07:50 -0300

    javascript adjustments

--- main.js
+++ main.js
@@ -2,38 +2,47 @@
 const openMenuBtn = document.querySelector('.open-menu');
 const navbar = document.querySelector('.navbar')
 const menuLinks = document.querySelectorAll('.menu-link')
-const menuBtn = document.querySelector('.menu .contact-btn')
+const menuContactBtn = document.querySelector('.menu .contact-btn')
 const backToTopBtn = document.querySelector('.back-to-top-btn')
 
-
-menuLinks.forEach(link =>{
-  link.addEventListener('click', () =>{
-  document.body.classList.remove('menu-expanded')
-  })
-})
-
-menuBtn.addEventListener('click', () =>{
-  document.body.classList.remove('menu-expanded')
-})
-
-
-openMenuBtn.addEventListener('click', ()=>{
-  document.body.classList.toggle('menu-expanded')
-})
-
-window.addEventListener('scroll', () =>{
+const showNavbar = () =>{
   if(scrollY > 0){
     navbar.classList.add('scroll')
   }else{
     navbar.classList.remove('scroll')
   }
+}
 
+const showBackToTopButton = () =>{
   if(scrollY > 550){
     backToTopBtn.classList.add('change')
   }else{
     backToTopBtn.classList.remove('change')
   }
+}
+
+function onScroll(){
+  showNavbar()
+  showBackToTopButton()
+}
+
+onScroll()
+
+menuLinks.forEach(link =>{
+  link.addEventListener('click', () =>{
+    document.body.classList.remove('menu-expanded')
+  })
 })
+
+menuContactBtn.addEventListener('click', () =>{
+  document.body.classList.remove('menu-expanded')
+})
+
+openMenuBtn.addEventListener('click', ()=>{
+  document.body.classList.toggle('menu-expanded')
+})
+
+window.addEventListener('scroll', onScroll)
 
 //Swiper
 const swiper = new Swiper('.swiper-container', {
