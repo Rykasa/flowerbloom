f67756b1e5b0a8473deeacf240a211ce7269a072
LAK <email@lak.com>
2022-05-11 12:34:43 -0300

    navbar OK

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 //Open navbar
 const openMenuBtn = document.querySelector('.open-menu');
+const navbar = document.querySelector('.navbar')
+const menuLinks = document.querySelectorAll('.menu-link')
+const menuBtn = document.querySelector('.menu .contact-btn')
+
+menuLinks.forEach(link =>{
+  link.addEventListener('click', () =>{
+  document.body.classList.remove('menu-expanded')
+  })
+})
+
+menuBtn.addEventListener('click', () =>{
+  document.body.classList.remove('menu-expanded')
+})
+
 
 openMenuBtn.addEventListener('click', ()=>{
   document.body.classList.toggle('menu-expanded')
 })
 
+window.addEventListener('scroll', () =>{
+  if(scrollY > 0){
+    navbar.classList.add('scroll')
+  }else{
+    navbar.classList.remove('scroll')
+
+  }
+})
 
 //Swiper
 const swiper = new Swiper('.swiper-container', {
