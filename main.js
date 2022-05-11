e2c7ccf6c1a9f77b0d888e004fd9a87ccb2f0469
LAK <email@lak.com>
2022-05-11 20:25:06 -0300

    section 1 responsive

--- main.js
+++ main.js
@@ -54,12 +54,12 @@ const swiper = new Swiper('.swiper-container', {
   },
   mousewheel: true,
   keyboard: true,
-  breakpoints: {
-        767: {
-            slidesPerView: 2,
-            setWrapperSize: true
-        }
-    }
+  // breakpoints: {
+  //       767: {
+  //           slidesPerView: 2,
+  //           setWrapperSize: true
+  //       }
+  //   }
 });
 
 //ScrollReveal
